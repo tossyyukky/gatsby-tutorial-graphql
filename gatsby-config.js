beae3f069be2e38d19476a7f178bc88f2043754a
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-04-06 00:47:31 +0900

    add: query

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
